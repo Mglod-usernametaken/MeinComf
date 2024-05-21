3ade95e2288571621a990a5035e6b19003ca40c5
marcin.glod <marcin.glod@programisci.eu>
2024-05-21 16:00:17 +0200

    work refinements

--- .config/nvim/lua/plugins/vimwiki.lua
+++ .config/nvim/lua/plugins/vimwiki.lua
@@ -1,15 +1,25 @@
 return {
-  {
-    "vimwiki/vimwiki",
-    lazy = false,
-    init = function()
-      vim.g.vimwiki_list = {
-        { path = '~/vimwiki', syntax = 'markdown', ext = '.wiki', links_space_char = '-' },
-        { path = '~/st/pm/work_notes', syntax = 'markdown', ext = '.md', links_space_char = '-' },
-      }
-      vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown', ['.markdown'] = 'markdown', ['.mdown'] = 'markdown' }
-      vim.g.vimwiki_use_mouse = 1
-      vim.g.vimwiki_markdown_link_ext = 1
-    end,
-  },
+	{
+		"vimwiki/vimwiki",
+		lazy = false,
+		init = function()
+			vim.g.vimwiki_list = {
+				{
+					path = "/mnt/c/Users/marcin.glod/Documents/brain",
+					syntax = "markdown",
+					ext = ".md",
+					links_space_char = "-",
+				},
+				{
+					path = "~/vimwiki",
+					syntax = "markdown",
+					ext = ".wiki",
+					links_space_char = "-",
+				},
+			}
+			vim.g.vimwiki_ext2syntax = { [".md"] = "markdown", [".markdown"] = "markdown", [".mdown"] = "markdown" }
+			vim.g.vimwiki_use_mouse = 1
+			vim.g.vimwiki_markdown_link_ext = 1
+		end,
+	},
 }
