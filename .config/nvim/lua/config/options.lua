780035a4db215c01745bf3ffd5819390037a841a
marcin.glod <marcin.glod@programisci.eu>
2024-06-04 14:57:10 +0200

    fix divergent aliases issue, add template for secret-aliases

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -19,12 +19,11 @@ opt.showmatch = true
 opt.relativenumber = true
 opt.number = true
 opt.termguicolors = true
-opt.colorcolumn = '100'
+opt.colorcolumn = "100"
 opt.signcolumn = "yes"
 opt.cmdheight = 1
 opt.scrolloff = 7
 opt.completeopt = "menuone,noinsert,noselect"
-
 
 -- Behaviour
 opt.hidden = true
