f09c898f36d4af1978945a9772970227a332aac9
marcin.glod <marcin.glod@programisci.eu>
2024-08-06 16:00:33 +0200

    refactor lspconfig

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,119 +1,122 @@
 local config = function()
-    local lspconfig = require("lspconfig")
-    local on_attach = require("util.lsp").on_attach
+	local lspconfig = require("lspconfig")
+	local on_attach = require("util.lsp").on_attach
 
-    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-    for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
-    end
+	local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
 
-    --lua
-    lspconfig.lua_ls.setup({
-        --capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-            Lua = {
-                --make the language server recognize "vim" global
-                diagnostics = {
-                    globals = { "vim" },
-                },
-                workspace = {
-                    library = {
-                        [vim.fn.expand ("$VIMRUNTIME/lua")] = true,
-                        [vim.fn.stdpath("config") .. "/lua"] = true,
-                    },
-                },
-            },
-        },
-    })
-    -- Python
-    lspconfig.pyright.setup({
-        --capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-            pyright = {
-                disableOrganizeImports = false,
-                analysis = {
-                    useLibraryCodeForTypes = true,
-                    autoSearchPaths = true,
-                    diagnosticMode = "workspace",
-                    autoImportCompletions = true,
-                },
-            },
-        },
-    })
-    -- Bash
-    lspconfig.bashls.setup({
-        --capabilities = capabilities,
-        on_attlch = on_attach,
-        filetypes = { "sh", "aliasrc" },
-    })
-    -- Yaml
-    lspconfig.yamlls.setup({
-        --capabilities = capabilities,
-        on_attach = on_attach,
-        filetypes = { "yaml", "yml", "Dockerfile" }
-    })
+	--lua
+	lspconfig.lua_ls.setup({
+		--capabilities = capabilities,
+		on_attach = on_attach,
+		settings = {
+			Lua = {
+				--make the language server recognize "vim" global
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.stdpath("config") .. "/lua"] = true,
+					},
+				},
+			},
+		},
+	})
+	-- Python
+	lspconfig.pyright.setup({
+		--capabilities = capabilities,
+		on_attach = on_attach,
+		settings = {
+			pyright = {
+				disableOrganizeImports = false,
+				analysis = {
+					useLibraryCodeForTypes = true,
+					autoSearchPaths = true,
+					diagnosticMode = "workspace",
+					autoImportCompletions = true,
+				},
+			},
+		},
+	})
+	-- Bash
+	lspconfig.bashls.setup({
+		--capabilities = capabilities,
+		on_attlch = on_attach,
+		filetypes = { "sh", "aliasrc" },
+	})
+	-- Yaml
+	lspconfig.yamlls.setup({
+		--capabilities = capabilities,
+		on_attach = on_attach,
+		filetypes = { "yaml", "yml", "Dockerfile" },
+	})
 
-    local luacheck = require("efmls-configs.linters.luacheck")
-    local stylua = require("efmls-configs.formatters.stylua")
-    local flake8 = require("efmls-configs.linters.flake8")
-    local black = require("efmls-configs.formatters.black")
-    local shellcheck = require("efmls-configs.linters.shellcheck")
-    local shfmt = require("efmls-configs.formatters.shfmt")
-    local prettier = require("efmls-configs.formatters.prettier")
-    local actionlint = require("efmls-configs.linters.actionlint")
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
+	local flake8 = require("efmls-configs.linters.flake8")
+	local black = require("efmls-configs.formatters.black")
+	local shellcheck = require("efmls-configs.linters.shellcheck")
+	local shfmt = require("efmls-configs.formatters.shfmt")
+	local prettier = require("efmls-configs.formatters.prettier")
+	local actionlint = require("efmls-configs.linters.actionlint")
+	local markdownlint = require("efmls-configs.linters.markdownlint")
 
-    --configure efm server
-    lspconfig.efm.setup({
-        filetypes = {
-            "lua",
-            "python",
-            "sh",
-            "yaml",
-            "yml",
-        },
-        init_options = {
-            documentFormatting = true,
-            documentRangeFormatting = true,
-            hover = true,
-            documentSymbol = true,
-            codeAction = true,
-            completion = true,
-        },
-        settings = {
-            languages = {
-                lua = { luacheck, stylua },
-                python = { flake8, black },
-                sh = { shellcheck, shfmt },
-                yaml = { prettier, actionlint }
-            },
-        },
-    })
+	--configure efm server
+	lspconfig.efm.setup({
+		filetypes = {
+			"lua",
+			"python",
+			"sh",
+			"yaml",
+			"yml",
+			"md",
+		},
+		init_options = {
+			documentFormatting = true,
+			documentRangeFormatting = true,
+			hover = true,
+			documentSymbol = true,
+			codeAction = true,
+			completion = true,
+		},
+		settings = {
+			languages = {
+				lua = { luacheck, stylua },
+				python = { flake8, black },
+				sh = { shellcheck, shfmt },
+				yaml = { prettier, actionlint },
+				markdown = { prettier, markdownlint },
+			},
+		},
+	})
 
-    local lsp_fmt_group = vim.api.nvim_create_augroup("LspFromattingGroup", {})
-    vim.api.nvim_create_autocmd("BufWritePost", {
-        group = lsp_fmt_group,
-        callback = function()
-            local efm = vim.lsp.get_active_clients({"efm"})
+	local lsp_fmt_group = vim.api.nvim_create_augroup("LspFromattingGroup", {})
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = lsp_fmt_group,
+		callback = function()
+			local efm = vim.lsp.get_active_clients({ "efm" })
 
-            if vim.tbl_isempty(efm) then
-                return
-            end
+			if vim.tbl_isempty(efm) then
+				return
+			end
 
-            vim.lsp.buf.format({"efm"})
-        end,
-    })
+			vim.lsp.buf.format({ "efm" })
+		end,
+	})
 end
 
 return {
-    "neovim/nvim-lspconfig",
-    config = config,
-    lazy = false,
-    dependencies = {
-        "windwp/nvim-autopairs",
-        "williamboman/mason.nvim",
-        "creativenull/efmls-configs-nvim",
-    },
+	"neovim/nvim-lspconfig",
+	config = config,
+	lazy = false,
+	dependencies = {
+		"windwp/nvim-autopairs",
+		"williamboman/mason.nvim",
+		"creativenull/efmls-configs-nvim",
+	},
 }
